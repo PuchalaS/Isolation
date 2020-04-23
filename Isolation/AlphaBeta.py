1605d35198bdad48bd3e10469dd7e9b6a904cbce
KowalikJakub <kowalik.kub@gmail.com>
2020-04-23 19:46:05 +0200

    Completed algorithm

--- Isolation/AlphaBeta.py
+++ Isolation/AlphaBeta.py
@@ -1,51 +1,71 @@
 import numpy as np
 from typing import Callable
-from Board import Board
 
 
 class AlphaBeta(object):
     def __init__(self,
                  board: np.ndarray,
                  depth: int,
-                 state_eval_fn: Callable[[np.ndarray], float]):
+                 player: int,
+                 enemy: int,
+                 eval_function_factory):
         self.board = board
         self.depth = depth
         self.__alpha = (-1) * np.inf
         self.__beta = np.inf
-        self.evaluate = state_eval_fn
+        self.player = player
+        self.enemy = enemy
+        self.__inital_player_position = find_player_position(
+            self.board, player)
+        self.__inital_enemy_position = find_player_position(self.board, enemy)
+        self.__player_state_eval_fn = eval_function_factory(2, 3)
+        self.__enemy_state_eval_fn = eval_function_factory(3, 2)
+
+    def update_state(self, state: np.ndarray) -> None:
+        self.state = state
 
     def predict_state(self):
         def AlphaBetaMax(state, alpha, beta, depth_left):
             if depth_left == 0:
-                return (state, self.evaluate(state))
-
-            for possible_state in possible_states(state, 2):
-                found_state, score = AlphaBetaMin(
+                return (state, self.__player_state_eval_fn(state))
+            best_state = None
+            best_value = -np.inf
+            for possible_state in possible_states(state, self.player, self.enemy):
+                _, score = AlphaBetaMin(
                     possible_state,
                     alpha,
                     beta,
                     depth_left - 1)
-                if score >= beta:
-                    return (found_state, beta)
-                if score > alpha:
-                    alpha = score
+
+                if score > best_value:
+                    best_value = score
+                    best_state = possible_state
+                alpha = max(alpha, best_value)
+                if beta <= alpha:
+                    break
+            return (best_state, best_value)
 
         def AlphaBetaMin(state, alpha, beta, depth_left):
             if depth_left == 0:
-                return (state, (-1) * self.evaluate(state))
-
-            for possible_state in possible_states(state, 3):
-                found_state, score = AlphaBetaMin(
+                return (state, (-1) * self.__enemy_state_eval_fn(state))
+            best_state = None
+            best_value = np.inf
+            for possible_state in possible_states(state, self.enemy, self.player):
+                _, score = AlphaBetaMax(
                     possible_state,
                     alpha,
                     beta,
                     depth_left - 1)
-                if score <= alpha:
-                    return (found_state, alpha)
-                if score < beta:
-                    beta = score
+                if score < best_value:
+                    best_value = score
+                    best_state = possible_state
+                beta = min(beta, best_value)
+                if beta <= alpha:
+                    break
+            return (best_state, best_value)
 
-        return AlphaBetaMax(self.board, self.__alpha, self.__beta, self.depth)
+        return AlphaBetaMax(self.board.copy(), self.__alpha, self.__beta, self.depth)
+
 
 
 def MeasureOneToTwoFactory(first_player: int, second_player: int):
@@ -62,23 +82,26 @@ def count_possible_states(state: np.ndarray, player: int) -> int:
     return first_move * second_move
 
 
-def possible_states(state: np.ndarray, current_player: int):
+def possible_states(state: np.ndarray, current_player: int, enemy_player: int):
     possible_moves = find_possible_player_moves(state, current_player)
     for move_vector in possible_moves:
         partial_state = move_player(state, current_player, move_vector)
-        possible_removals = find_possible_removals(partial_state)
+        possible_removals = find_possible_removals(partial_state, enemy_player)
         for remove_pos in possible_removals:
             yield remove_square(partial_state, remove_pos)
 
 
 def find_possible_player_moves(state: np.ndarray, current_player: int) -> list:
-    pos_x, pos_y = np.where(state == current_player)
-    pos_x = pos_x[0]
-    pos_y = pos_y[0]
+    pos_x, pos_y = find_player_position(state, current_player)
     player_neighbourhood = state[pos_x - 1: pos_x + 2, pos_y - 1: pos_y + 2]
     x_positions = (np.where(player_neighbourhood == 1)[0] + pos_x - 1)
     y_positions = (np.where(player_neighbourhood == 1)[1] + pos_y - 1)
     return np.array((x_positions, y_positions)).T.tolist()
+
+
+def find_player_position(state: np.ndarray, player: int) -> tuple:
+    pos_x, pos_y = np.where(state == player)
+    return pos_x[0], pos_y[0]
 
 
 def player_neighbourhood(state: np.ndarray, player: int) -> np.ndarray:
@@ -88,14 +111,20 @@ def player_neighbourhood(state: np.ndarray, player: int) -> np.ndarray:
     return state[pos_x - 1: pos_x + 2, pos_y - 1: pos_y + 2]
 
 
-def find_possible_removals(state: np.ndarray) -> list:
-    return np.array(np.where(state == 1)).T.tolist()
+def find_possible_removals(state: np.ndarray, player: int) -> list:
+    player_pos = np.array(np.where(state == player)).T[0]
+    res = np.array(np.where(state == 1)).T
+    diff = res - player_pos
+    distance = np.sqrt(np.sum(diff*diff, axis=1))
+    asc_order_indexes = np.argsort(distance)
+    return res[asc_order_indexes]
 
 
 def move_player(state: np.ndarray, current_player: int, move_vector: tuple) -> np.ndarray:
     result = state.copy()
+    pos_x, pos_y = find_player_position(state, current_player)
     result[move_vector[0], move_vector[1]] = current_player
-    result[move_vector[0], move_vector[1]] = 1
+    result[pos_x, pos_y] = 1
     return result
 
 
@@ -103,3 +132,25 @@ def remove_square(state: np.ndarray, square_pos: tuple) -> np.ndarray:
     result = state.copy()
     result[square_pos[0], square_pos[1]] = 0
     return result
+
+
+def print_state(state: np.ndarray):
+    print(state[1:-1, 1:-1])
+
+
+__state = np.zeros((9, 9))
+state = np.ones((7, 7))
+__state[1:8, 1:8] = 1
+state = __state
+
+player_position = (1, 4)
+enemy_position = (7, 4)
+state[player_position] = 2
+state[enemy_position] = 3
+
+states = list(possible_states(state, 2, 3))
+alpha_beta = AlphaBeta(state, 4, 2, 3, MeasureOneToTwoFactory)
+player_eval = MeasureOneToTwoFactory(2, 3)
+enemy_eval = MeasureOneToTwoFactory(3, 2)
+state = alpha_beta.predict_state()
+print(state)
--- Isolation/Player.py
+++ Isolation/Player.py
@@ -1,4 +1,16 @@
-class Player():
+class Agent():
+    def fetch_action(self) -> tuple:
+        pass
+
+
+class AlphaBetaAgent(Agent):
+    def __init__(self):
+        self.__agent = AlphaBeta(...)
+
+    def fetch_action(self):
+        return self.agent.predict_action()
+
+class Player(Agent):
 
     def __init__(self, name, is_white, start_x, start_y):
         self.start_x = start_x
@@ -10,3 +22,8 @@ class Player():
     
     def return_position(self):
         return (self.pos_x, self.pos_y)
+
+    def fetch_action(self):
+        player_input = input()
+        #parse
+        return ((1,2),(2,3))
