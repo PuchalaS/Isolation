b0b09fcdb767d15585b53fc12c5d9aacd095ad78
iterb70 <sebastian.puchala.95@gmail.com>
2020-04-23 22:17:50 +0200

    Player, Game and Test class update

---
+++ Isolation/Game.py
@@ -0,0 +1,28 @@
+﻿import numpy as np
+import os
+import platform
+from Board import Board
+from Player import HumanPlayer, RandomPlayer, SemiRandomPlayer
+
+def clear():
+    current_platform = platform.system()
+    if current_platform == 'Darwin':
+        os.system('clear')
+    elif current_platform == 'Windows':
+        os.system('cls')
+    elif current_platform == 'Linux':
+        os.system('clear')
+
+def new_game(board):
+
+    board.init_players_pos();
+    while (board.is_active_player_lost()==False):
+        board = board.active_player.fetch_action(board)
+        clear()
+        board.print_board()
+        board.switch_turn()
+    
+    print("Przegral - " +board.active_player.name)
+
+    return board.active_player.is_white
+
---
+++ Isolation/Tests.py
@@ -0,0 +1,45 @@
+﻿import numpy as np
+from Board import Board
+from Player import HumanPlayer, RandomPlayer, SemiRandomPlayer
+from Game import new_game
+
+
+
+def random_vs_radom(cycles):
+    results= []
+    for x in range(cycles):
+        white = RandomPlayer("Biały", True, 6, 3)
+        black = RandomPlayer("Czarny", False, 0, 3)
+        board = Board(white, black)
+        results.append(new_game(board))
+    black_wins = np.count_nonzero(results)/cycles * 100
+    white_wins = 100 - black_wins
+    print ("Bialy wygral: "+ str(white_wins)+"%")
+    print ("Czarny wygral: "+ str(black_wins)+"%")
+
+def random_vs_semi_radom(cycles):
+    results= []
+    for x in range(cycles):
+        white = RandomPlayer("Biały", True, 6, 3)
+        black = SemiRandomPlayer("Czarny", False, 0, 3)
+        board = Board(white, black)
+        results.append(new_game(board))
+    black_wins = np.count_nonzero(results)/cycles * 100
+    white_wins = 100 - black_wins
+    print ("Bialy wygral: "+ str(white_wins)+"%")
+    print ("Czarny wygral: "+ str(black_wins)+"%")
+
+def semi_random_vs_semi_radom(cycles):
+    results= []
+    for x in range(cycles):
+        white = SemiRandomPlayer("Biały", True, 6, 3)
+        black = SemiRandomPlayer("Czarny", False, 0, 3)
+        board = Board(white, black)
+        results.append(new_game(board))
+    black_wins = np.count_nonzero(results)/cycles * 100
+    white_wins = 100 - black_wins
+    print ("Bialy wygral: "+ str(white_wins)+"%")
+    print ("Czarny wygral: "+ str(black_wins)+"%")
+
+
+semi_random_vs_semi_radom(10)  
